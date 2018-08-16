7b23a8e6c8507da895a637eac5066e389735a246
Thomas Parisot <oncletom@users.noreply.github.com>
2018-08-16 22:35:24 +0200

    Bon ben map via des doto

---
+++ js/geojson-map.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const $$ = (selector) => Array.from(document.querySelectorAll(selector));
+
+  $$('.geojson-map').forEach(el => {
+    const map = L.map(el);
+    map.setView([46.66362, 0.93952], 6);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    }).addTo(map);
+
+    const jsonData = JSON.parse(decodeURI(el.dataset.geojson));
+
+    L.geoJson(jsonData, {
+      pointToLayer: (point, latlng) => {
+        const classes = [];
+        const {people} = point.properties;
+
+        if (!people || (people.match(/noémie/i) && people.match(/thomas/i))){
+          classes.push('with--both');
+        }
+        else if (people && people.match(/noémie/i)) {
+          classes.push('with--noemie');
+        }
+        else if (people && people.match(/thomas/i)) {
+          classes.push('with--thomas');
+        }
+
+        const icon = L.divIcon({
+          html: '<svg class="geo-marker" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><use xlink:href="/images/marker.svg#Layer_1"></use></svg>',
+          iconUrl: '/images/marker.svg',
+          className: classes.join(' '),
+          iconSize: 20,
+        });
+
+        return L.marker(latlng, { icon });
+      },
+    }).addTo(map);
+  });
+
+});
